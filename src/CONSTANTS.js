e765968107caa1e20c32bb8f775582a269a22785
wrunion <winterrunion@gmail.com>
2020-08-09 15:53:45 -0700

    Initial commit.

---
+++ src/CONSTANTS.js
@@ -0,0 +1,23 @@
+export const TITLE = "Title Here";
+
+export const SUB_TITLE = "Sub heading here";
+
+export const INTRO_PARAGRAPH = "Include instructions and longer description of your application here.";
+
+export const SERVICES = [
+  "Rent Assistance", "Utility Assistance", "Health Care", "OHP Application", "Food Boxes", "Food Pantries", "Education", "Unemployment Assistance", "Disability Services", "Child Care", "Legal Aid", "Immigration Services", "Non-Citizen Services", "Domestic Violence Survivor Services", "Sexual Assault Support Services", "Mental Health Care", "Support Groups", "Counseling", 
+]
+
+export const INCOME_CATEGORIES = [];
+
+export const CHECKBOXES = [
+  "Pregnant", "Disabled", "Veteran", "LGBTQ", "Farm Worker", "Older Adult (over 65)"
+]
+
+export const DEMOGRAPHIC_GROUPS = [
+  "African American/Black", "Immigrant", "Arab American", "Jewish", "Latino/Hispanic", "Native American/Indigenous", "Asian American", "Other (please specify below)", 
+]
+
+export const FOOTER_PARAGRAPH = "More instructions, details, or disclaimers here."
+
+export const COPYRIGHT_INFO = "Copyright (c) Your Name Here, 2020"

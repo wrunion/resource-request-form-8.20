48e309f8fda3c056074538398b26ff1e20ea4314
wrunion <winterrunion@gmail.com>
2020-08-09 19:04:03 -0700

    Build out form

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,9 +7,9 @@ export default class Form extends React.Component {
       <div className="Form">
         <div className="Who">
         <h3>I'm looking for resources for: </h3>
-          <input type="checkbox" id="myself" name="client" />Myself
-          <input type="checkbox" id="myself" name="client" />A Client
-          <input type="checkbox" id="myself" name="client" />Other
+          <input type="checkbox" id="myself" name="client" /> Myself
+          <input type="checkbox" id="myself" name="client" /> A Client
+          <input type="checkbox" id="myself" name="client" /> Other
         </div>
         <div className="Where">
           <h3>Located In:</h3>
@@ -18,6 +18,12 @@ export default class Form extends React.Component {
             <h3>Service types:</h3>
             <p>(Check any that apply)</p>
             <div className="Services">{SERVICES.sort().map((e,i) => <><input type="checkbox" name="serviceType" className="Service" value={e} id={i} key={i} text={e}/> {e}  </>)}</div>
+            <br />
+            <label>Other: </label><br/>
+            <textarea
+              placeholder="List other services needed here"
+              rows={3}
+              ></textarea>
           </div>
           <div className="Demographics1">
             <h3>The person looking for services is: </h3>
@@ -28,7 +34,9 @@ export default class Form extends React.Component {
             <div className="Services">{DEMOGRAPHIC_GROUPS.sort().map((e,i) => <><input type="checkbox" name="serviceType" className="Service" value={e} id={i} key={i} text={e}/> {e}  </>)}</div>
           </div>
         </div>
-
+        <div className="Income">
+            <h3>Income (optional): <input type="text" id="incomeInput" placeholder="Approximate Income"/></h3>
+          </div>
       </div>
     )
   }
